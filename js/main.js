7be1aeff92d1eca925d00b562da20b5b89dfcea2
AdolKW <KW2261387@outlook.com>
2023-10-06 14:49:53 +0800

    优化导航栏

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
-function createNavItem(href, text) {
-    return `<li><a href="${href}">${text}</a></li>`;
-}
+function CreateHeader() {
+    let headerDiv = document.createElement('div');
+    headerDiv.className = 'header';
 
-function Init() {
-    // 创建 nav 元素
-    var nav = document.createElement('nav');
+    let navWrapperDiv = document.createElement('div');
+    navWrapperDiv.className = 'nav';
 
-    // 创建 ul 元素
-    var ul = document.createElement('ul');
+    let titleA = document.createElement('a');
+    titleA.className = 'title';
+    titleA.href='/index.html';
+    titleA.textContent='伊始之苏';
+    headerDiv.appendChild(titleA);
 
-    // 创建 li 元素和 a 元素，并设置相关属性和链接
-    var items = [
-        { href: '/index.html', text: '主页' },
+    let ul = document.createElement('ul');
+
+    let items = [
+        { href: '/index.html', text: '首页' },
         { href: '/navigation/收藏.html', text: '收藏' },
-        { href: '/navigation/工具箱.html', text: '工具箱' }
+        { href: '/navigation/工具.html', text: '工具' }
     ];
 
-    // 创建 li 元素和 a 元素，并设置相关属性和链接
-    var navItems = items.map(item => createNavItem(item.href, item.text)).join('');
+    items.forEach(item => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.href = item.href;
+        a.textContent = item.text;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
 
-    // 将 li 元素添加到 ul 元素中
-    ul.innerHTML = navItems;
+    navWrapperDiv.appendChild(ul);
+    headerDiv.appendChild(navWrapperDiv);
 
-    // 将 ul 元素添加到 nav 元素中
-    nav.appendChild(ul);
-
-    // 将 nav 元素添加到页面的 body 标签中
-    document.body.appendChild(nav);
+    document.body.innerHTML = '';
+    document.body.appendChild(headerDiv);
 }
 
-Init();
-
+CreateHeader();
