d40ddf4b33d14239d3550b5cbb8401c4149b7d68
AdolKW <KW2261387@outlook.com>
2023-12-15 20:25:24 +0800

    大更新

--- js/main.js
+++ js/main.js
@@ -1,59 +1,91 @@
 function CreateHeader() {
-    let headerDiv = document.createElement('div');
-    headerDiv.className = 'header';
+    const headerDiv = document.createElement('div')
+    headerDiv.className = 'header'
 
-    let headerImage = document.createElement('img');
-    headerImage.className = "header-image";
-    headerImage.src = '/image/headerImage.png';
-    headerImage.alt = "加载失败";
+    const headerImage = document.createElement('img')
+    headerImage.className = "header-image"
+    headerImage.src = '/image/headerImage.png'
+    headerImage.alt = "加载失败"
 
-    let imageLink = document.createElement('a');
-    imageLink.href = '/index.html';
-    imageLink.appendChild(headerImage);
-    headerDiv.appendChild(imageLink);
+    const imageLink = document.createElement('a')
+    imageLink.href = '/index.html'
+    imageLink.appendChild(headerImage)
+    headerDiv.appendChild(imageLink)
 
-    let navWrapperDiv = document.createElement('div');
-    navWrapperDiv.className = 'nav';
-    let ul = document.createElement('ul');
-    let items = [
+    const navWrapperDiv = document.createElement('div')
+    navWrapperDiv.className = 'nav'
+    const ul = document.createElement('ul')
+    const items = [
         { href: '/index.html', text: '首页' },
         { href: '/navigation/collection.html', text: '收藏' },
         { href: '/navigation/tool.html', text: '工具' }
     ];
     items.forEach(item => {
-        let li = document.createElement('li');
-        let a = document.createElement('a');
-        a.href = item.href;
-        a.textContent = item.text;
-        if(window.location.pathname===item.href){
-            a.classList.add('a-active');
+        const li = document.createElement('li')
+        const a = document.createElement('a')
+        a.href = item.href
+        a.textContent = item.text
+        if (window.location.pathname === item.href) {
+            a.classList.add('a-active')
         }
-        li.appendChild(a);
-        ul.appendChild(li);
+        li.appendChild(a)
+        ul.appendChild(li)
     });
-    let sliderDiv = document.createElement('div');
-    sliderDiv.className = 'slider';
-    ul.appendChild(sliderDiv);
+    const sliderDiv = document.createElement('div')
+    sliderDiv.className = 'slider'
+    ul.appendChild(sliderDiv)
 
-    let titleA = document.createElement('a');
-    titleA.className = 'title';
-    titleA.href = '/index.html';
-    titleA.textContent = '伊始之苏';
-    navWrapperDiv.appendChild(titleA);
+    const titleA = document.createElement('a')
+    titleA.className = 'title'
+    titleA.href = '/index.html'
+    titleA.textContent = '伊始之苏'
+    navWrapperDiv.appendChild(titleA)
 
-    navWrapperDiv.appendChild(ul);
-    headerDiv.appendChild(navWrapperDiv);
+    navWrapperDiv.appendChild(ul)
+    headerDiv.appendChild(navWrapperDiv)
 
-    document.body.innerHTML = '';
-    document.body.appendChild(headerDiv);
+    document.body.innerHTML = ''
+    document.body.appendChild(headerDiv)
 }
-CreateHeader();
+CreateHeader()
 
 function AddSiteIcon() {
-    let link = document.createElement('link');
-    link.rel = 'shortcut icon';
-    link.href = '/image/ys.png';
-    link.type = 'image/x-icon';
-    document.head.appendChild(link);
+    const link = document.createElement('link')
+    link.rel = 'shortcut icon'
+    link.href = '/image/ys.png'
+    link.type = 'image/x-icon'
+    document.head.appendChild(link)
 }
-AddSiteIcon();
+AddSiteIcon()
+
+function CreateFloatBtn() {
+    const toTopBtn = document.createElement('div')
+    toTopBtn.id = 'to-top'
+    toTopBtn.className = 'float-btn'
+    toTopBtn.innerHTML = '<span>↑</span>'
+
+    const toBottomBtn = document.createElement('div')
+    toBottomBtn.id = 'to-bottom'
+    toBottomBtn.className = 'float-btn'
+    toBottomBtn.innerHTML = '<span>↓</span>'
+
+    document.body.appendChild(toTopBtn)
+    document.body.appendChild(toBottomBtn)
+
+
+    toTopBtn.addEventListener('click', () => scrollToPosition('.base-container', 'top'))
+    toBottomBtn.addEventListener('click', () => scrollToPosition('.base-container', 'bottom'))
+
+    function scrollToPosition(target, position) {
+        const element = document.querySelector(target)
+        const scrollHeight = element.scrollHeight
+        const containerHeight = element.clientHeight
+        const maxScrollTop = scrollHeight - containerHeight
+
+        element.scrollTo({
+            top: position === 'top' ? 0 : maxScrollTop,
+            behavior: 'smooth'
+        })
+    }
+}
+window.addEventListener('load', CreateFloatBtn)
